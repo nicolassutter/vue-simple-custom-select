2a4bd11e2138829eb1c4efe00377b2883c979667
Nicolas <nicolas@alsacreations.fr>
2021-04-27 10:43:47 +0200

    Declare Option type

--- vue-simple-select.d.ts
+++ vue-simple-select.d.ts
@@ -1,4 +1,6 @@
 import Vue, { PluginFunction, VueConstructor } from 'vue';
+import { Option } from './src/interfaces/options'
 
 declare const VueSimpleSelect: VueConstructor<Vue> & { install: PluginFunction<any>; };
+export declare const Option: Option
 export default VueSimpleSelect;
